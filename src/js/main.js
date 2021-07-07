83caa5d327c494ac82f3ccb97160692832c790c9
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-07-07 07:30:38 +0100

    Updated

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ var myInit = {
 
 let FederalData = new Request("/institutes/data/universities/Federal.json");
 let StateData = new Request("/institutes/data/universities/State.json");
+let PrivateData = new Request("/institutes/data/universities/Private.json");
 
 const FederalUniversitiesData = fetch(FederalData).then((res) => {
   return res.json().then((data) => {
@@ -58,6 +59,32 @@ const StateUniversitiesData = fetch(StateData).then((res) => {
       //   close for loop
 
       document.querySelector("#StateUniversities").innerHTML = temp;
+    }
+  });
+});
+
+const PrivateUniversitiesData = fetch(PrivateData).then((res) => {
+  return res.json().then((data) => {
+    console.log(data);
+    if (data.length > 0) {
+      var temp = "";
+
+      //   start for loop
+
+      data.forEach((u) => {
+        temp += ` 
+            <div class="university">
+                <div class="university__image">
+                    <img src="${u.logo}" alt="" />
+                </div>
+                <div class="university__name">${u.name}</div>
+            </div>
+        `;
+      });
+
+      //   close for loop
+
+      document.querySelector("#PrivateUniversities").innerHTML = temp;
     }
   });
 });
